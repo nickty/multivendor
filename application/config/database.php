44a5af42ec4fbf8dcc96367a695ad18503faf718
Mizanur Nickty PC <nickty.86@gmail.com>
2020-06-04 17:19:04 +0600

    database updated

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'multivendor';
+$db['default']['username'] = 'khujfsht_root';
+$db['default']['password'] = 'Nick126721';
+$db['default']['database'] = 'khujfsht_multivendor';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
